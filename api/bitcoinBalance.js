e81d7a22c936e11558fb9d9ad979c9b123957e33
Bitq2323 <152601486+Bitq2323@users.noreply.github.com>
2024-01-20 02:40:45 +0300

    Update bitcoinBalance.js

--- api/bitcoinBalance.js
+++ api/bitcoinBalance.js
@@ -15,27 +15,26 @@ function getScriptHash(address) {
 
     try {
         if (address.startsWith('1')) {
-            const { hash } = bitcoinjs.address.fromBase58Check(address);
-            // Construct the output script manually for P2PKH addresses
-            output = bitcoinjs.script.compile([
-                bitcoinjs.opcodes.OP_DUP,
-                bitcoinjs.opcodes.OP_HASH160,
+            const { hash } = bitcoin.address.fromBase58Check(address);
+            output = bitcoin.script.compile([
+                bitcoin.opcodes.OP_DUP,
+                bitcoin.opcodes.OP_HASH160,
                 hash,
-                bitcoinjs.opcodes.OP_EQUALVERIFY,
-                bitcoinjs.opcodes.OP_CHECKSIG
+                bitcoin.opcodes.OP_EQUALVERIFY,
+                bitcoin.opcodes.OP_CHECKSIG
             ]);
         } else if (address.startsWith('3')) {
-            const payment = bitcoinjs.payments.p2sh({
+            const payment = bitcoin.payments.p2sh({
                 address: address,
-                network: bitcoinjs.networks.bitcoin,
+                network: bitcoin.networks.bitcoin,
             });
             output = payment.output;
         } else if (address.startsWith('bc1')) {
-            const bech32Decoded = bitcoinjs.address.fromBech32(address);
+            const bech32Decoded = bitcoin.address.fromBech32(address);
             if (bech32Decoded.data.length === 20) {
-                output = bitcoinjs.payments.p2wpkh({ hash: bech32Decoded.data }).output;
+                output = bitcoin.payments.p2wpkh({ hash: bech32Decoded.data }).output;
             } else if (bech32Decoded.data.length === 32) {
-                output = bitcoinjs.payments.p2wsh({ hash: bech32Decoded.data }).output;
+                output = bitcoin.payments.p2wsh({ hash: bech32Decoded.data }).output;
             }
         }
     } catch (err) {
@@ -48,7 +47,7 @@ function getScriptHash(address) {
         return null;
     }
 
-    const scriptHash = bitcoinjs.crypto.sha256(output);
+    const scriptHash = bitcoin.crypto.sha256(output);
     return Buffer.from(scriptHash).reverse().toString('hex');
 }
 
